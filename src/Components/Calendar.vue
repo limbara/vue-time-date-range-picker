bb1f15b1360790fea286c548d72bb43e0fda8230
Nico <limbaranico@yahoo.com>
2020-06-30 00:11:28 +0700

    add in progress calendar

--- src/Components/Calendar.vue
+++ src/Components/Calendar.vue
@@ -7,23 +7,16 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td v-for="i in 7" :key={i}>{{ i }}</td>
-        </tr>
-        <tr>
-          <td v-for="i in 7" :key={i}>{{ i }}</td>
-        </tr>
-        <tr>
-          <td v-for="i in 7" :key={i}>{{ i }}</td>
-        </tr>
-        <tr>
-          <td v-for="i in 7" :key={i}>{{ i }}</td>
-        </tr>
-        <tr>
-          <td v-for="i in 7" :key={i}>{{ i }}</td>
-        </tr>
-        <tr>
-          <td v-for="i in 7" :key={i}>{{ i }}</td>
+        <tr v-for="row in 6" :key="row">
+          <td
+            v-for="day in getRowDays(row)"
+            :key="day.timestamp"
+            :class="{
+              highlighted: day.isInRange || day.isStartDate || day.isEndDate,
+            }"
+          >
+            {{ day.dateNumber }}
+          </td>
         </tr>
       </tbody>
     </table>
@@ -33,20 +26,137 @@
 <script>
 import DateUtil from '../Utils/DateUtil';
 
+class Day {
+  constructor(date, isInRange, isStartDate, isEndDate, isDisabled) {
+    this.date = date;
+    this.timestamp = date.getTime();
+    this.dateNumber = date.getDate();
+    this.isInRange = isInRange;
+    this.isStartDate = isStartDate;
+    this.isEndDate = isEndDate;
+    this.isDisabled = isDisabled;
+  }
+}
+
 export default {
   props: {
     selectedStartDate: Date,
     selectedEndDate: Date,
     language: String,
+    disabledDates: Object,
   },
   data() {
+    const dateUtil = new DateUtil(this.language);
+    const startDate = this.selectedStartDate ?? this.selectedEndDate ?? new Date();
+    const pageTimestamp = dateUtil.toUnix(dateUtil.startOf(startDate, 'month'));
+
     return {
-      dateUtil: new DateUtil(this.language),
+      // stored page time stamp start of a month
+      pageTimestamp,
+      dateUtil,
     };
   },
   computed: {
     dayNames() {
       return this.dateUtil.getAbbrDayNames();
+    },
+    days() {
+      let pageDate = this.dateUtil.fromUnix(this.pageTimestamp);
+      const daysInMonth = this.dateUtil.daysInMonth(pageDate);
+      const days = [];
+      const preDays = [];
+      const postDays = [];
+
+      for (let i = 0; i < daysInMonth; i += 1) {
+        days.push(this.constructDay(pageDate));
+        pageDate = this.dateUtil.add(pageDate, 1, 'd');
+      }
+
+      let firstDay = days[0].date;
+
+      for (let j = this.dateUtil.weekday(firstDay); j > 0; j -= 1) {
+        firstDay = this.dateUtil.subtract(firstDay, 1, 'd');
+        preDays.push(this.constructDay(firstDay));
+      }
+
+      let lastDay = days[days.length - 1].date;
+
+      for (let k = preDays.length + days.length; k < 42; k += 1) {
+        lastDay = this.dateUtil.add(lastDay, 1, 'd');
+        postDays.push(this.constructDay(lastDay));
+      }
+
+      return [...preDays, ...days, ...postDays];
+    },
+  },
+  methods: {
+    getRowDays(row) {
+      const endIndex = row * 7;
+      const startIndex = endIndex - 7;
+
+      return this.days.slice(startIndex, endIndex);
+    },
+    constructDay(date) {
+      return new Day(
+        date,
+        this.dateUtil.isInRange(
+          date,
+          this.selectedStartDate,
+          this.selectedEndDate,
+        ),
+        this.dateUtil.isSameDate(date, this.selectedStartDate),
+        this.dateUtil.isSameDate(date, this.selectedEndDate),
+        this.isDisabledDate(date),
+      );
+    },
+    isDisabledDate(date) {
+      if (typeof this.disabledDates === 'undefined') {
+        return false;
+      }
+      let disabled = false;
+      const {
+        dates, from, to, ranges, custom,
+      } = this.disabledDates;
+
+      if (typeof dates !== 'undefined' && Array.isArray(dates)) {
+        dates.forEach((d) => {
+          if (this.DateUtil.isSameDate(d, date)) {
+            disabled = true;
+          }
+        });
+        if (disabled) return true;
+      }
+
+      if (ranges !== 'undefined') {
+        ranges.forEach((range) => {
+          if (
+            this.dateUtil.isValidDate(range.from)
+            && this.dateUtil.isValidDate(range.to)
+          ) {
+            if (this.dateUtil.isInRange(date, range.from, range.to)) {
+              disabled = true;
+            }
+          }
+        });
+        if (disabled) return true;
+      }
+
+      if (this.dateUtil.isValidDate(to) && this.dateUtil.isBefore(date, to)) {
+        disabled = true;
+      }
+
+      if (
+        this.dateUtil.isValidDate(from)
+        && this.dateUtil.isAfter(date, from)
+      ) {
+        disabled = true;
+      }
+
+      if (typeof custom === 'function' && custom(date)) {
+        disabled = true;
+      }
+
+      return disabled;
     },
   },
 };
--- src/Utils/DateUtil.js
+++ src/Utils/DateUtil.js
@@ -4,13 +4,14 @@ export default class {
   /**
    * @param {String} lang
    */
-  constructor(lang) {
+  constructor(lang = '') {
     this.lang = lang;
     this.localMoment = moment().locale(lang);
   }
 
   /**
    * Get Day Names
+   *
    * @returns {Array}
    */
   getDayNames() {
@@ -19,6 +20,7 @@ export default class {
 
   /**
    * Get Abbreviated Day Names
+   *
    * @returns {Array}
    */
   getAbbrDayNames() {
@@ -35,6 +37,7 @@ export default class {
 
   /**
    * Get Abbreviated Month Names
+   *
    * @returns {Array}
    */
   getAbbrMonthNames() {
@@ -49,17 +52,36 @@ export default class {
    * @returns {String}
    */
   formatDate(date, format) {
-    return moment(date).locale(this.lang).format(format);
+    return moment(date)
+      .locale(this.lang)
+      .format(format);
   }
 
   /**
    * check for same date
+   *
    * @param {Date} date1
    * @param {Date} date2
+   * @return {Boolean}
    */
   // eslint-disable-next-line class-methods-use-this
   isSameDate(date1, date2) {
-    return moment(date1).format('DD MM yyyy') === moment(date2).format('DD MM yyyy');
+    return (
+      moment(date1).format('DD MM yyyy') === moment(date2).format('DD MM yyyy')
+    );
+  }
+
+  /**
+   * check if a date is between 2 date
+   *
+   * @param {Date} date
+   * @param {Date} fromDate
+   * @param {Date} toDate
+   * @returns {Boolean}
+   */
+  // eslint-disable-next-line class-methods-use-this
+  isInRange(date, fromDate, toDate) {
+    return moment(date).isBetween(fromDate, toDate);
   }
 
   /**
@@ -70,6 +92,136 @@ export default class {
    */
   // eslint-disable-next-line class-methods-use-this
   isValidDate(date) {
-    return moment(date).isValid();
+    return (
+      Object.prototype.toString.call(date) === '[object Date]'
+      && moment(date).isValid()
+    );
+  }
+
+  /**
+   * convert date to unix timestamp
+   *
+   * @param {Date} date
+   * @returns {Number}
+   */
+  // eslint-disable-next-line class-methods-use-this
+  toUnix(date) {
+    return moment(date).unix();
+  }
+
+  /**
+   * convert unix to date
+   *
+   * @param {Number} unixTimestamp
+   * @returns {Date}
+   */
+  // eslint-disable-next-line class-methods-use-this
+  fromUnix(unixTimestamp) {
+    return moment.unix(unixTimestamp).toDate();
+  }
+
+  /**
+   * Get Start Of A Date. refer to moment documentation
+   *
+   * @param {Date} date
+   * @param {String} of
+   * @returns {Date}
+   */
+  // eslint-disable-next-line class-methods-use-this
+  startOf(date, of) {
+    return moment(date)
+      .startOf(of)
+      .toDate();
+  }
+
+  /**
+   * Get End Of A Date. refer to moment documentation
+   *
+   * @param {Date} date
+   * @param {String} of
+   * @returns {Date}
+   */
+  // eslint-disable-next-line class-methods-use-this
+  endOf(date, of) {
+    return moment(date)
+      .endOf(of)
+      .toDate();
+  }
+
+  /**
+   * Check if date is before a date
+   *
+   * @param {Date} date
+   * @param {Date} beforeDate
+   * @returns {Boolean}
+   */
+  // eslint-disable-next-line class-methods-use-this
+  isBefore(date, beforeDate) {
+    return moment(date).isBefore(beforeDate);
+  }
+
+  /**
+   * Check if date is after a date
+   *
+   * @param {Date} date
+   * @param {Date} beforeDate
+   * @returns {Boolean}
+   */
+  // eslint-disable-next-line class-methods-use-this
+  isAfter(date, beforeDate) {
+    return moment(date).isAfter(beforeDate);
+  }
+
+  /**
+   * Add Number of timeKey to A Date. refer to moment documentation
+   *
+   * @param {Date} date
+   * @param {Number} number
+   * @param {String} timeKey
+   * @returns {Date}
+   */
+  // eslint-disable-next-line class-methods-use-this
+  add(date, number, timeKey) {
+    return moment(date)
+      .add(number, timeKey)
+      .toDate();
+  }
+
+  /**
+   * Subtract Number of timeKey to A Date. refer to moment documentation
+   *
+   * @param {Date} date
+   * @param {Number} number
+   * @param {String} timeKey
+   * @returns {Date}
+   */
+  // eslint-disable-next-line class-methods-use-this
+  subtract(date, number, timeKey) {
+    return moment(date)
+      .subtract(number, timeKey)
+      .toDate();
+  }
+
+  /**
+   * Get Number of Day in A month from date
+   *
+   * @param {Date} date
+   * @returns {Number}
+   */
+  // eslint-disable-next-line class-methods-use-this
+  daysInMonth(date) {
+    return moment(date).daysInMonth();
+  }
+
+  /**
+   * Get Day Number 0 - 6 from A Date
+   *
+   * @param {Date} date
+   * @returns {Number}
+   */
+  weekday(date) {
+    return moment(date)
+      .locale(this.lang)
+      .weekday();
   }
 }
